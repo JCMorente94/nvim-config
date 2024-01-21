7946a05e104badc8b43f8fa323dd102a2267a3ba
javi <jcmorente94@gmail.com>
2024-01-21 19:58:55 +0100

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,41 +1,41 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-git",
-    --"hrsh7th/cmp-nvim-lsp-signature-help",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "windwp/nvim-autopairs",
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = {
-        ["<C-j>"] = cmp.mapping.select_next_item(),
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-y>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Insert,
-          select = true,
-        }),
-        ["<c-space>"] = cmp.mapping.complete(),
-      },
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "path" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        --{ name = 'nvim_lsp_signature_help' }
-      },
-    })
-  end,
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/cmp-git",
+		--"hrsh7th/cmp-nvim-lsp-signature-help",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"windwp/nvim-autopairs",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = {
+				["<C-j>"] = cmp.mapping.select_next_item(),
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+				["<C-y>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+				["<c-space>"] = cmp.mapping.complete(),
+			},
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				--{ name = 'nvim_lsp_signature_help' }
+			},
+		})
+	end,
 }
