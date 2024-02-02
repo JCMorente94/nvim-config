806cea075f3286207eab191ea5b51b6af2699936
javi <jcmorente94@gmail.com>
2024-02-02 20:17:52 +0100

    add html and typescript

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright" },
+				ensure_installed = { "lua_ls", "pyright" , "zls", "tsserver", "html" },
 			})
 		end,
 	},
@@ -19,6 +19,9 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			lspconfig.pyright.setup({})
+      lspconfig.zls.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.html.setup({})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
